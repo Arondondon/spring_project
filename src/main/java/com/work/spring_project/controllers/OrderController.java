3ca1ad8a5c8e8b97f887b42e1b6205c862218c2d
Dronny <andreypravdin52@gmail.com>
2023-01-15 17:23:49 +0300

    Fixed bugs with Order

--- src/main/java/com/work/spring_project/controllers/OrderController.java
+++ src/main/java/com/work/spring_project/controllers/OrderController.java
@@ -11,9 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 /**
  * Service (choice)
@@ -41,6 +42,8 @@ public class OrderController {
     @Autowired
     private OrderRepo orderRepo;
 
+    private Order curOrder;
+
     @GetMapping("/order")
     public String main(@AuthenticationPrincipal User user, Model model) {
         Iterable<Service> services = serviceRepo.findAll();
@@ -52,15 +55,45 @@ public class OrderController {
     }
 
     @PostMapping("/order")
-    public String addOrder(@AuthenticationPrincipal User user, @RequestParam Car car, @RequestParam Service service,
+    public String orderForm(@AuthenticationPrincipal User user, @RequestParam Car car, @RequestParam Service service,
                            @RequestParam String deliveryFrom, @RequestParam String deliveryTo,
-                           @RequestParam boolean outOfTown, @RequestParam boolean oversized,
+                           @RequestParam(required = false) String outOfTown, @RequestParam(required = false) String oversized,
                            @RequestParam String commentary, Model model) {
 
-        Order order = new Order(user, service, car, deliveryTo, deliveryFrom, commentary, outOfTown, oversized);
-        order.setTotal_price(order.calculatePrice());
-        orderRepo.save(order);
+        boolean delOutOfTown;
+        if(outOfTown != null) {
+            delOutOfTown = true;
+        } else {
+            delOutOfTown = false;
+        }
 
-        return "redirect: /";
+        boolean oversizedDel;
+        if(oversized != null) {
+            oversizedDel = true;
+        } else {
+            oversizedDel = false;
+        }
+
+        Order order = new Order(user, service, car, deliveryTo, deliveryFrom, commentary, delOutOfTown, oversizedDel);
+        order.setTotal_price(order.calculatePrice());
+        curOrder = order;
+
+        return "redirect:/order/confirm";
     }
+
+    @GetMapping("/order/confirm")
+    public String confirm(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("order", curOrder);
+
+        return "confirm";
+    }
+
+    @PostMapping("/order/confirm")
+    public String addOrder(Model model) {
+        orderRepo.save(curOrder);
+
+        return "redirect:/";
+    }
+
 }
